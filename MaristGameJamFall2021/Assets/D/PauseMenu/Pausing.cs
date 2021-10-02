ecb35d4cb689b67b32238937bd0e4164a7267781
Daniel <Daniel.Degen1@marist.edu>
2021-10-01 22:33:42 -0400

    Main Menu Pause Menu and End Menu base versions made

---
+++ MaristGameJamFall2021/Assets/D/PauseMenu/Pausing.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausing : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    public GameObject player;
+    public GameObject pauseMenu;
+    public bool isPaused;
+    // Start is called before the first frame update
+
+    void Start() {
+        playerInput = player.GetComponent<PlayerInput>();
+    }
+
+    void Awake()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInput.Pause)
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+    }
+    public void GoToMainMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ MaristGameJamFall2021/Assets/D/Start and End Menu/WOOHOO/End.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    public void Mainmenu()
+    {
+            {
+                SceneManager.LoadScene(2);
+            }
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
